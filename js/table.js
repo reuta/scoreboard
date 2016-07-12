64540167c974129b0835c0b45856a13e3947ff4d
Adrian Reuter <reutadri@icloud.com>
2016-07-12 18:54:42 +0200

    made sort function working

---
+++ js/table.js
@@ -0,0 +1,44 @@
+var playerTable = document.querySelector("#players");
+var playerInput = document.querySelector("#player-name");
+var addButton = document.querySelector("#add-player");
+
+// table methods
+
+function addPlayer(){
+	var playerName = playerInput.value;
+	playerInput.value = "";
+	playerTable.innerHTML += "<tr class='player'><th id='player-name'>" + playerName + "</th><th id='player-points' contenteditable></th></tr>";
+}
+
+function getPlayers(){
+	return playerTable.querySelectorAll(".player");
+}
+
+function sort(){
+	var players = getPlayers();
+	var points = Array.prototype.map.call(players, function(el){
+		return {
+			element: el.cloneNode(true),
+			points: parseInt(el.querySelector("#player-points").innerHTML)
+		}
+	});
+	
+	points.sort(function(a, b){
+		return b.points - a.points;
+	});
+	
+	Array.prototype.forEach.call(players, function(player, i){
+		var parentNode = player.parentNode;
+		parentNode.removeChild(player);
+		parentNode.appendChild(points[i].element);
+		console.log(player);
+		console.log(parentNode);
+	});
+	
+
+}
+
+
+// event binding
+addButton.addEventListener("click", addPlayer);
+
